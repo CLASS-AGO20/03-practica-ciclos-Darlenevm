9848be01eb021bd38e965fab0e3957c795c236b9
Darlenevm <dmoreno9@ucol.mx>
2020-12-10 16:23:19 -0600

    factoriales

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+        let i, total=1;
+        for(i = 1; i<=numero; i++){
+            total = total * i
+        }
+        return total;
+    }
+}
 
 let app = new App();
+console.log(app.factorial(5));
+console.log(app.factorial(8));
