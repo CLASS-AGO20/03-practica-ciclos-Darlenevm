0505f31fb4aa5694c752f445b36df1a05164a3f4
Darlenevm <dmoreno9@ucol.mx>
2020-12-10 16:38:49 -0600

    string

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
 export default class App {
 
     factorial(numero){
-        let i, total=1;
-        for(i = 1; i<=numero; i++){
+        let i, total = 1;
+        for(i = 1; i<= numero; i++){
             total = total * i
         }
         return total;
     }
+    convertirAString(numero){
+        let i = 1;
+        let string = "";
+        while (i<=numero){
+            string +='*';
+            i++;
+        }
+        return string;
+    }
+
 }
 
 let app = new App();
 console.log(app.factorial(5));
 console.log(app.factorial(8));
+console.log("");
+console.log(app.convertirAString(3));
+
